a7656ae8f397d9b8fd6e8054e195866eeb7cf561
MOMAN AMJAD <momanamjad07@gmail.com>
2025-12-24 10:34:45 +0500

    check

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,10 @@ import App from './App.jsx'
 import AuthContext from './context/AuthContext.jsx'
 import TaskContext from './context/TaskContext.jsx'
 createRoot(document.getElementById('root')).render(
+
 <AuthContext>
 <TaskContext>
+
        <App />
 
 </TaskContext>
