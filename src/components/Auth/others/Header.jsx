467a86a6cbe26a7a34883075cd5c848f86bd9867
MOMAN AMJAD <momanamjad07@gmail.com>
2026-01-08 21:11:25 +0500

    changes

--- src/components/Auth/others/Header.jsx
+++ src/components/Auth/others/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({ changeUser, data }) => {
 
   return (
     <div className="flex items-end justify-between">
-      <h1 className="text-2xl font-medium">Hello <span className="text-3xl font-semibold">{userName}</span></h1>
+      <h1 className="text-2xl font-medium">Hello  mr.<span className="text-3xl font-semibold">{userName}</span></h1>
       <button onClick={logoutUser} className="bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm cursor-pointer">Logout</button>
     </div>
   );
